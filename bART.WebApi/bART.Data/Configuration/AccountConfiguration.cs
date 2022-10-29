7dd741046e91b352486ae7ef6b244b5297baaa45
Dammy9 <nazar.demchyk.ki.2020@lpnu.ua>
2022-10-29 21:07:54 +0300

    Removed unnecessary usings

--- bART.WebApi/bART.Data/Configuration/AccountConfiguration.cs
+++ bART.WebApi/bART.Data/Configuration/AccountConfiguration.cs
@@ -1,10 +1,5 @@
 ﻿using bART.Data.Entities;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace bART.Data.Configuration
 {
--- bART.WebApi/bART.Data/Configuration/IncidentConfiguration.cs
+++ bART.WebApi/bART.Data/Configuration/IncidentConfiguration.cs
@@ -1,12 +1,5 @@
 ﻿using bART.Data.Entities;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace bART.Data.Configuration
 {
